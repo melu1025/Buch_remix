c7e839a35abf57cdfdc2b7977c524eff9eabb2de
abda1014 <127962371+abda1014@users.noreply.github.com>
2023-12-30 18:53:36 +0100

    style:update

--- app/component/searchPage.tsx
+++ app/component/searchPage.tsx
@@ -1,64 +1,35 @@
-import { Flex, Input, Select, Button, Text } from "@chakra-ui/react";
-import { useState,FC } from "react";
-interface StarRatingProps {
-    value: number;
-    onChange: (rating: number) => void;
-}
+import { Flex, Input, Select, Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import StarsRating from './stars';
 
-const StarRating: FC<StarRatingProps> = ({ value, onChange }) => {
-    const handleClick = (rating:number) => {
-        onChange(rating);
-    };
-
-    return (
-        <Flex>
-            <Text fontSize="xl" mr={2}>
-                Bewertung:
-            </Text>
-            {[1, 2, 3, 4, 5].map((rating) => (
-                <Text
-                    key={rating}
-                    fontSize="xl"
-                    cursor="pointer"
-                    onClick={() => handleClick(rating)}
-                    color={value >= rating ? "yellow.500" : "gray.300"}
-                    transition="color 0.2s"
-                    display="inline-block"
-                    mr={1}
-                >
-                    ★
-                </Text>
-            ))}
-        </Flex>
-    );
-};
 
 export default function Search() {
-    const [selectedRating, setSelectedRating] = useState(0);
+  const [selectedRating, setSelectedRating] = useState(0);
 
-    const handleRatingChange = (rating: number) => {
-        setSelectedRating(rating);
-    };
+  const handleRatingChange = (rating: number) => {
+    setSelectedRating(rating);
+  };
 
-    return (
-        <Flex direction="column" align="center" justify="center" height="100vh">
-            <form>
-                <Flex direction="row" align="center" justify="center" mb="4">
-                    <Input type="text" placeholder="Titel" mr="2" />
-                    <Input type="text" placeholder="Schlagwörter" />
-                </Flex>
-
-                <Flex direction="row" align="center" justify="center" mb="4">
-                    <Select placeholder="Art" width="100%">
-                        <option>Druckausgabe</option>
-                        <option>Kindle</option>
-                    </Select>
-                </Flex>
-                <StarRating value={selectedRating} onChange={handleRatingChange} />
-                <Button mt="4" colorScheme="teal" type="submit">
-                    Suche
-                </Button>
-            </form>
+  return (
+    <Flex direction="column" align="center" justify="center" height="100vh">
+      <form>
+        <Flex direction="row" align="center" justify="center" mb="4">
+          <Input type="text" placeholder="Titel" mr="2" />
+          <Input type="text" placeholder="Schlagwörter" />
         </Flex>
-    );
+
+        <Flex direction="row" align="center" justify="center" mb="4">
+          <Select placeholder="Art" width="100%">
+            <option>Druckausgabe</option>
+            <option>Kindle</option>
+          </Select>
+        </Flex>
+        <StarsRating> value={selectedRating} onChange={handleRatingChange} <StarsRating/>
+        <Button mt="4" colorScheme="teal" type="submit">
+          Suche
+        </Button>
+      </form>
+    </Flex>
+  );
 }
+
