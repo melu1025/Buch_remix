37742b56a9208405ee1e718db3d0bfed6127e6ac
Lukas Merkle <melu1025@h-ka.de>
2023-12-30 21:44:52 +0100

    feature: error-handling for search

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -4,12 +4,22 @@ import HorizontalBar from "~/component/bar";
 import Barstyle from '~/component/bar.css';
 import '~/component/searchPage.css';
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { Button, Flex, Input, Select } from "@chakra-ui/react";
+import { Button, Flex, Input, Select,} from "@chakra-ui/react";
 import BookTable from "~/component/book.table";
+import axios from 'axios';
+import https from 'https';
+// import fs from 'fs';
+// cert: fs.readFileSync('app/certificate/certificate.crt')
 
 export async function fetchBuch(suchkriterien: string){
-    const res = await fetch(`https://localhost:3000/rest/?${suchkriterien}`)
-    return await res.json();
+    const res = await axios(`https://localhost:3000/rest/?${suchkriterien}`, {
+      httpsAgent: new https.Agent({rejectUnauthorized: false}),
+      validateStatus :function(status){
+        return status < 500;
+      }
+    })
+    console.log(res.data)
+    return res.data;
   }
   
   export async function action({
@@ -21,18 +31,25 @@ export async function fetchBuch(suchkriterien: string){
   [...formData.entries()].forEach(([key, value]) => {
       if (value === '') formData.delete(key);
   });
-  
+    
     const queryString = new URLSearchParams(formData as any).toString();
     console.log('formData:', queryString);
-    const buchData = await fetchBuch(queryString);
-    console.log('buecher:', buchData?._embedded?.buecher);
+    const buchData =   await fetchBuch(queryString);
+     console.log('buecher:', buchData);
     return buchData;
   }
 
 export default function Search() {
+  // const [flag, setFlag] =  useBoolean(true);
 
-    const data = useActionData<typeof action>();
-
+  const data = useActionData<typeof action>();
+  let buecher = data;
+  if(data && data?._embedded){
+    buecher = data?._embedded?.buecher;
+  }else if(data && data.statusCode == 404){
+    buecher = [];
+  }
+    
   return (
       <div>
           <main>
@@ -50,11 +67,11 @@ export default function Search() {
                         <option>KINDLE</option>
                     </Select>
                 </Flex>
-                <Button mt="4" colorScheme="teal" type="submit">
-                    Suche
+                <Button mt="4" colorScheme="teal" type="submit" >
+                    Suche 
                 </Button>
             </Form>
-            {<BookTable {...data?._embedded?.buecher}></BookTable> }
+            <BookTable buchArray={buecher} ></BookTable>     
         </Flex>
           <Outlet />
           </main>
