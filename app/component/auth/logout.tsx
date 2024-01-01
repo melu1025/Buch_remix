97da0763f26dd29b3ec973d6c28520c67552b1e9
Finn Lamberti <lafi1011@h-ka.de>
2024-01-01 14:05:24 +0100

    login bugfix

--- app/component/auth/logout.tsx
+++ app/component/auth/logout.tsx
@@ -1,8 +1,25 @@
-const handleLogout = () => {
+import { Button } from "@chakra-ui/react";
+import { useAuth } from './AuthContext';
+
+const LogoutComponent: React.FC = () => {
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
     // Token und Rolle aus dem LocalStorage entfernen
     localStorage.removeItem('token');
     localStorage.removeItem('role');
-    console.log('Erfolgreich ausgeloggt');
+    logout();
+    console.log('Erfolgreich ausgeloggt:');
+    alert('Sie wurden erfolgreich ausgeloggt');
   };
 
-  export default handleLogout;
+  return (
+    <>
+      <Button colorScheme='red' onClick={handleLogout}>
+        Logout
+      </Button>
+    </>
+  );
+};
+
+export default LogoutComponent;
