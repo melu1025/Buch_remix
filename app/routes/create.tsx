0221dea6ba7977b1f39925a5193dd11ae54f0ae9
mopa1021 <148717572+mopa1021@users.noreply.github.com>
2024-01-04 23:25:00 +0100

    feature: New way of handling errors

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -1,64 +1,29 @@
 import type {ActionFunctionArgs} from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import NewBook from "~/component/new-book"; 
-import newbook from "~/component/new-book.css";
-import {Outlet } from "@remix-run/react";
+import NewBook from "~/component/new.book"; 
+import newbook from "~/component/new.book.css";
+import {Outlet, useActionData } from "@remix-run/react";
 import HorizontalBar from "~/component/bar";
 import barstyle from '~/component/bar.css';
-import https from 'node:https';
-import fs from 'node:fs';
-import axios from "axios";
+import { postBuch } from '~/service/book.service';
+import BookError from '~/component/book.error';
 
 export default function Create() {
+
+  const actionData = useActionData<typeof action>();
+
   return (
     <div>
       <HorizontalBar title={'Anlegen'} subtitle={'Mehr als nur Worte: Dein Buch, unsere Reise'}></HorizontalBar>
       <main>
+          <BookError responseStatus={actionData}></BookError>
           <NewBook />
           <Outlet />
       </main>
+      
     </div>
   );
 }
-
-export async function postBuch(objektDaten :object, tokenDatei:string, ) {
-
-  const serverUrl = 'https://localhost:3000';
-
-    return await axios.post (`${serverUrl}/rest`,
-    objektDaten,
-    {headers: {
-      ContentType: 'application/json',
-      Authorization: `Bearer ${tokenDatei}`,
-  }, httpsAgent: new https.Agent({ca: fs.readFileSync('app/certificate/certificate.crt')}),
-    // validateStatus :function(status){
-    //   return true;
-    // }
-  })
-  .then(function (response){
-    console.log('Server response:', response.status);
-    return response;
-  })
-  .catch(function (error){
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-      return error.response;
-    } else if (error.request) {
-      // The request was made but no response was received
-      // error.request is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request);
-      return;
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-      return;
-    }})
-    }
 
 export async function action({ request } : ActionFunctionArgs ) {
   try {
@@ -84,7 +49,7 @@ export async function action({ request } : ActionFunctionArgs ) {
       art: formData.get('buchArt'), 
       preis: Number.parseFloat(formData.get('preis')?.toString() || ''), 
       rabatt: Number.parseFloat(formData.get('rabatt')?.toString() || ''),
-      lieferbar: formData.get('lieferbar') === 'on', // Checkbox: true, wenn aktiviert, sonst false
+      lieferbar: formData.get('lieferbar') === 'on',
       datum: formData.get('datum'),
       homepage: formData.get('homepage'),
       schlagwoerter: schlagwoerterArray,
@@ -102,17 +67,22 @@ export async function action({ request } : ActionFunctionArgs ) {
 
     const response = await postBuch(buchDaten, token);
 
-    const responseBody = response?.data;
-    console.log('ResponseBdy:', responseBody);
     console.log('Server response:', response);
 
-    console.log('Server Status response:', response?.status);
-
-    if ( response.status === 201){
+    switch (response.status) {
+    case 201: {
       return redirect('/success');
     }
-    else if ( response.status === 422) {
-      return redirect('/error');
+    case 403: {
+      return response.status;
+    }
+    case 422: {
+      return response.status;
+    }
+    default: { if (response === 500) {
+      return response;
+    }
+    }
     }
     } catch (error:any) {
     console.error('Fehler beim Speichern des Buchs:', error);
