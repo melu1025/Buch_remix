7a1fa7e67ef3dd44b1e3eeb100c25395815b2a47
mopa1021 <148717572+mopa1021@users.noreply.github.com>
2024-01-05 00:18:26 +0100

    style: prettier

--- app/component/book.error.tsx
+++ app/component/book.error.tsx
@@ -1,41 +1,47 @@
-import { Flex, Alert, AlertDescription, AlertIcon, AlertTitle } from "@chakra-ui/react";
+import {
+  Flex,
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+} from '@chakra-ui/react';
 
-export default function BookError({
-    responseStatus,
-}: any) {
-    if (!responseStatus) return <div></div>;
+export default function BookError({ responseStatus }: any) {
+  if (!responseStatus) return <div></div>;
 
-    let alertTitle = "";
-    let alertDescription = "";
+  let alertTitle = '';
+  let alertDescription = '';
 
-    switch (responseStatus) {
+  switch (responseStatus) {
     case 403: {
-        alertTitle = "Sie sind nicht angemeldet";
-        alertDescription = "Bitte melden Sie sich zuerst an. Vorher kann kein neues Buch angelegt";
-    
-    break;
+      alertTitle = 'Sie sind nicht angemeldet';
+      alertDescription =
+        'Bitte melden Sie sich zuerst an. Vorher kann kein neues Buch angelegt';
+
+      break;
     }
     case 422: {
-        alertTitle = "Das Buch existiert bereits";
-        alertDescription = "Es existiert bereits ein Buch mit dieser ISBN.";
-    
-    break;
+      alertTitle = 'Das Buch existiert bereits';
+      alertDescription = 'Es existiert bereits ein Buch mit dieser ISBN.';
+
+      break;
     }
     case 500: {
-        alertTitle = "Der Server ist nicht erreichbar";
-        alertDescription = "Bitte versuchen Sie es später noch einmal. Vielen Dank";
-    
-    break;
-    }
-    }
+      alertTitle = 'Der Server ist nicht erreichbar';
+      alertDescription =
+        'Bitte versuchen Sie es später noch einmal. Vielen Dank';
 
-    return (
-        <Flex align="center" justify="center">
-            <Alert status="error" flexDirection="column" textAlign="center">
-                <AlertIcon />
-                <AlertTitle>{alertTitle}</AlertTitle>
-                <AlertDescription>{alertDescription}</AlertDescription>
-            </Alert>
-        </Flex>
-    );
+      break;
+    }
+  }
+
+  return (
+    <Flex align="center" justify="center">
+      <Alert status="error" flexDirection="column" textAlign="center">
+        <AlertIcon />
+        <AlertTitle>{alertTitle}</AlertTitle>
+        <AlertDescription>{alertDescription}</AlertDescription>
+      </Alert>
+    </Flex>
+  );
 }
